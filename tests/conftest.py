5da70d609b1af83cd2a19df58acab5cf35e6543f
Alexander Sosedkin <asosedkin@redhat.com>
2024-02-14 14:38:00 +0100

    WIP: turn async

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,24 +6,17 @@
 import getpass
 import typing
 
-import MySQLdb  # type: ignore[import-untyped]
+import asyncmy  # type: ignore[import-untyped]
 import pytest
 import pytest_mysql  # type: ignore[import-untyped]
 import pytest_mysql.config  # type: ignore[import-untyped]
 import pytest_mysql.exceptions  # type: ignore[import-untyped]
 import pytest_mysql.factories  # type: ignore[import-untyped]
 from _pytest.fixtures import FixtureRequest
-from MySQLdb import Connection, ProgrammingError
 
 PROCESS_T: typing.TypeAlias = (
     pytest_mysql.executor.MySQLExecutor
     | pytest_mysql.executor_noop.NoopMySQLExecutor
-)
-
-
-mysql_my_proc = pytest_mysql.factories.mysql_proc(
-    port=None,
-    user=getpass.getuser(),
 )
 
 
@@ -39,79 +32,48 @@ def _async_mysql(
 ) -> typing.Callable[[FixtureRequest], typing.Any]:
     """Client fixture factory for MySQL server. Async."""
 
-    def _connect(
-        connect_kwargs: dict[str, typing.Any],
-        query_str: str,
-        mysql_db: str,
-    ) -> MySQLdb.Connection:
-        """Apply given query to a  given MySQLdb connection."""
-        mysql_conn: MySQLdb.Connection = Connection(**connect_kwargs)
-        try:
-            mysql_conn.query(query_str)
-        except ProgrammingError as e:
-            if 'database exists' in str(e):
-                msg = f'Database {mysql_db} already exists.'
-                raise pytest_mysql.exceptions.DatabaseExists(msg) from e
-            raise
-        return mysql_conn
-
     @pytest.fixture()
-    def mysql_fixture(
+    async def mysql_fixture(
         request: FixtureRequest,
-    ) -> typing.Generator[MySQLdb.Connection, None, None]:
-        """Client fixture for MySQL server.
-
-        #. Get config.
-        #. Try to import MySQLdb package.
-        #. Connect to mysql server.
-        #. Create database.
-        #. Use proper database.
-        #. Drop database after tests.
-
-        :param request: fixture request object
-
-        :returns: connection to database
-        """
+    ) -> typing.AsyncGenerator[asyncmy.Connection, None]:
         config = pytest_mysql.config.get_config(request)
+        mysql_db = dbname or config['dbname']
+
+        # sync
         process: PROCESS_T = request.getfixturevalue(process_fixture_name)
         if not process.running():
             process.start()
 
-        mysql_user = process.user
-        mysql_passwd = passwd or config['passwd']
-        mysql_db = dbname or config['dbname']
-
+        assert process.unixsocket
         connection_kwargs: dict[str, str | int] = {
             'host': process.host,
-            'user': mysql_user,
-            'passwd': mysql_passwd,
+            'unix_socket': process.unixsocket,
+            'user': process.user,
+            'password': passwd or config['passwd'],
         }
-        if process.unixsocket:
-            connection_kwargs['unix_socket'] = process.unixsocket
-        else:
-            connection_kwargs['port'] = process.port
+        conn = await asyncmy.connect(**connection_kwargs)
 
         query_str = (
             f'CREATE DATABASE `{mysql_db}` '
             f'DEFAULT CHARACTER SET {charset} '
             f'DEFAULT COLLATE {collation}'
         )
-        mysql_conn: MySQLdb.Connection = _connect(
-            connection_kwargs,
-            query_str,
-            mysql_db,
-        )
-        mysql_conn.query(f'USE `{mysql_db}`')
-        yield mysql_conn
+        async with conn.cursor() as cur:
+            await cur.execute(query_str)
+            await cur.execute(f'USE `{mysql_db}`')
 
-        # clean up after test that forgot to fetch selected data
-        if not mysql_conn.open:
-            mysql_conn = Connection(**connection_kwargs)
-        mysql_conn.store_result()
-        mysql_conn.query(f'DROP DATABASE IF EXISTS `{mysql_db}`')
-        mysql_conn.close()
+        yield conn
+
+        async with conn.cursor() as cur:
+            await cur.execute(f'DROP DATABASE IF EXISTS `{mysql_db}`')
+        conn.close()
 
     return mysql_fixture
 
+
+mysql_my_proc = pytest_mysql.factories.mysql_proc(
+    port=None,
+    user=getpass.getuser(),
+)
 
 async_mysql = _async_mysql('mysql_my_proc', dbname='my')
--- tests/test_a.py
+++ tests/test_a.py
@@ -3,12 +3,10 @@
 
 """Test main module of asyncmy_vcrlike."""
 
-import MySQLdb.connections  # type: ignore[import-untyped]
+import asyncmy  # type: ignore[import-untyped]
 
 
-def test_mysql(async_mysql: MySQLdb.connections.Connection) -> None:
+async def test_mysql(async_mysql: asyncmy.Connection) -> None:
     """Run test."""
-    cur = async_mysql.cursor()
-    cur.execute('CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20));')
-    async_mysql.commit()
-    cur.close()
+    async with async_mysql.cursor() as cur:
+        await cur.execute('CREATE TABLE pet (name VARCHAR(20));')
